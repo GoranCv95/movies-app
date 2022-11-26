72202ea3baa28770ae0be93dfe0df8ea95d9f1c7
GoranCv95 <gorancv95@gmail.com>
2022-11-26 12:24:41 +0100

    Before updating CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,7 @@ import { SliderComponent } from './components/slider/slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
 import { ItemComponent } from './components/item/item.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-
+import {PaginatorModule} from 'primeng/paginator';
 
 @NgModule({
   declarations: [
@@ -31,7 +30,7 @@ import { NgxPaginationModule } from 'ngx-pagination';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxPaginationModule
+    PaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
