c739856fd3d1a62d7de14aab4c0cc02c7ee9d78d
GoranCv95 <gorancv95@gmail.com>
2022-11-26 13:49:01 +0100

    Pagination done
    Everything is working good

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -8,24 +8,19 @@ import { MoviesService } from '../../services/movies.service';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
-
   movies: Movie[] = [];
-  p: number = 1;
-  itemPerPage: number = 20;
-  totalMovies: any;
 
-  constructor(private moviesService: MoviesService) { }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-   this.retriveData();
+    this.getPagedMovies(1);
   }
 
-  retriveData(){
-    this.moviesService.searchMovies(2).subscribe((movies) => {
-      this.movies = movies;
-      console.log(movies);
-      this.totalMovies = movies.length
-    })
+  getPagedMovies(page: number){
+    this.moviesService.searchMovies(page).subscribe((movies) => (this.movies = movies));
   }
 
+  paginate(event: any){
+    this.getPagedMovies(event.page + 1);
+  }
 }
